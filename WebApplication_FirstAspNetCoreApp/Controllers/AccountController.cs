5fcf595b0d1eda765e065bf3556f2defb1544ba4
Berker ERTAN <berkerertan@hotmail.com>
2024-01-21 02:34:08 +0300

    Register,Login page eklendi

---
+++ WebApplication_FirstAspNetCoreApp/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_FirstAspNetCoreApp.Controllers
+{
+    public class AccountController : Controller
+    {
+      // /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //   /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- WebApplication_FirstAspNetCoreApp/Controllers/HomeController.cs
+++ WebApplication_FirstAspNetCoreApp/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace WebApplication_FirstAspNetCoreApp.Controllers
             return View();
         }
 
+        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
