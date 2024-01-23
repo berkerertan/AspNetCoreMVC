c140aceb662f1df7361c7a2c7ebace59cbc829d1
Berker ERTAN <berkerertan@hotmail.com>
2024-01-23 03:17:00 +0300

    Controller eklendi.

---
+++ WebApplication_FirstAspNetCoreApp/Controllers/WhatIsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_FirstAspNetCoreApp.Controllers
+{
+    public class WhatIsController : Controller
+    {
+        public IActionResult RazorSyntax()
+        {
+            return View();
+        }
+    }
+}
